494f67ca123e8b5f9f545e9964de690f3d708d79
binchoo <079111w@gmail.com>
2022-05-13 11:42:45 +0900

    Javadoc 수정

--- PaimonGanyu/domain/src/main/java/org/binchoo/paimonganyu/redeem/UserRedeem.java
+++ PaimonGanyu/domain/src/main/java/org/binchoo/paimonganyu/redeem/UserRedeem.java
@@ -22,8 +22,6 @@ public class UserRedeem {
     private boolean done;
 
     /**
-     * 이력이 {@code 완료(Completed)} 또는 {@code 중복(Duplicate)}으로 기록되었다면
-     * 관련 유저에게 리딤 코드가 잘 들어간 것이므로 {@code 완수(Done)}로 판단합니다.
      * @return 이 이력이 완수 이력인지 여부
      */
     public boolean isDone() {
