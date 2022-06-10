7c2573d4073c420f9fe50cb1956174f6d4116331
binchoo <079111w@gmail.com>
2022-06-10 22:40:46 +0900

    Write basic code of /ikakao/hoyopass/post

--- PaimonGanyu/application/src/main/java/org/binchoo/paimonganyu/chatbot/controller/HoyopassController.java
+++ PaimonGanyu/application/src/main/java/org/binchoo/paimonganyu/chatbot/controller/HoyopassController.java
@@ -1,22 +1,30 @@
 package org.binchoo.paimonganyu.chatbot.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.binchoo.paimonganyu.hoyopass.UserHoyopass;
 import org.binchoo.paimonganyu.hoyopass.driving.SecuredHoyopassRegistryPort;
 import org.binchoo.paimonganyu.ikakao.SkillPayload;
 import org.binchoo.paimonganyu.ikakao.SkillResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@ControllerAdvice
 @RequiredArgsConstructor
 @RestController("/ikakao/hoyopass")
-public class ChatbotController {
+public class HoyopassController {
 
-    private final SecuredHoyopassRegistryPort hoyopassRegistration;
+    private final SecuredHoyopassRegistryPort hoyopassRegistry;
 
     @PostMapping("/post")
     public ResponseEntity<SkillResponse> addHoyopass(@RequestBody SkillPayload skillPayload) {
+        String botUserId = skillPayload.userRequest.user.getId();
+        String securedHoyopass = skillPayload.userRequest.params.get("secured_hoyopass");
+        UserHoyopass registeredHoyopass = hoyopassRegistry.registerHoyopass(botUserId, securedHoyopass);
         return null;
     }
 
