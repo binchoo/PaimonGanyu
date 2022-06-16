b536807ba8ca07e94348990f3014d917c677feb9
binchoo <079111w@gmail.com>
2022-06-16 20:01:31 +0900

    #4 Test changeDataSwitch

--- PaimonGanyu/hoyoapi/src/main/java/org/binchoo/paimonganyu/hoyoapi/pojo/ChangeDataSwitchResult.java
+++ PaimonGanyu/hoyoapi/src/main/java/org/binchoo/paimonganyu/hoyoapi/pojo/ChangeDataSwitchResult.java
@@ -4,6 +4,6 @@ package org.binchoo.paimonganyu.hoyoapi.pojo;
  * @author : jbinchoo
  * @since : 2022-06-16
  */
-public interface ChangeDataSwitchResult {
+public class ChangeDataSwitchResult {
     // empty
 }
