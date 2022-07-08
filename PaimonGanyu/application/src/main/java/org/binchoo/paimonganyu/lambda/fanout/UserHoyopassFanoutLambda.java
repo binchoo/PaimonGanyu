688ae9a7fdc6f517822c3a8d958d5b232c185b36
binchoo <079111w@gmail.com>
2022-07-08 15:42:28 +0900

    Introduce AwsEventWrappingLambda<T>, apply this at hoyopass fanout domain

--- PaimonGanyu/application/src/main/java/org/binchoo/paimonganyu/lambda/fanout/UserHoyopassFanoutLambda.java
+++ PaimonGanyu/application/src/main/java/org/binchoo/paimonganyu/lambda/fanout/UserHoyopassFanoutLambda.java
@@ -7,7 +7,9 @@ import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.binchoo.paimonganyu.awsutils.AwsEventWrapper;
 import org.binchoo.paimonganyu.awsutils.support.AwsEventWrapperFactory;
+import org.binchoo.paimonganyu.awsutils.support.template.AsyncEventWrappingLambda;
 import org.binchoo.paimonganyu.hoyopass.UserHoyopass;
 import org.binchoo.paimonganyu.infra.hoyopass.dynamo.item.UserHoyopassItem;
 import org.binchoo.paimonganyu.lambda.dailycheck.dto.UserHoyopassMessage;
@@ -15,7 +17,7 @@ import org.binchoo.paimonganyu.lambda.dailycheck.dto.UserHoyopassMessage;
 /**
  * Fanout lambda should start fast, so do not use spring context.
  */
-public class UserHoyopassFanoutLambda {
+public class UserHoyopassFanoutLambda extends AsyncEventWrappingLambda<DynamodbEvent> {
 
     private static final String USERHOYOPASS_TOPIC = System.getenv("USERHOYOPASS_TOPIC");
 
@@ -23,10 +25,19 @@ public class UserHoyopassFanoutLambda {
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final AmazonSNS snsClient = AmazonSNSClientBuilder.defaultClient();
 
-    public void handler(DynamodbEvent dynamodbEvent) {
-        var factory = AwsEventWrapperFactory.getDefault();
-        var eventWrapper = factory.newWrapper(dynamodbEvent, dynamodbMapper);
-        eventWrapper.extractPojos(dynamodbEvent, UserHoyopassItem.class).stream()
+    @Override
+    protected void lookupDependencies() {
+        // inline-dependencies declared.
+    }
+
+    @Override
+    protected Object[] getConstructorArgs() {
+        return new Object[] { dynamodbMapper };
+    }
+
+    @Override
+    protected void doHandle(DynamodbEvent event, AwsEventWrapper<DynamodbEvent> eventWrapper) {
+        eventWrapper.extractPojos(event, UserHoyopassItem.class).stream()
                 .map(UserHoyopassItem::toDomain)
                 .map(this::createMessage)
                 .forEach(this::publish);
