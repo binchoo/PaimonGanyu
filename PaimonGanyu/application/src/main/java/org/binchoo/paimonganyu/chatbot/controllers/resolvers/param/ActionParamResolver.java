78c19597885cf4181b5ee80299009d95bae71be3
binchoo <079111w@gmail.com>
2022-07-14 11:05:21 +0900

    #17 ActionParamResolver extends RequestResponseBodyMethodProcessor

--- PaimonGanyu/application/src/main/java/org/binchoo/paimonganyu/chatbot/controllers/resolvers/param/ActionParamResolver.java
+++ PaimonGanyu/application/src/main/java/org/binchoo/paimonganyu/chatbot/controllers/resolvers/param/ActionParamResolver.java
@@ -4,23 +4,33 @@ import lombok.RequiredArgsConstructor;
 import org.binchoo.paimonganyu.chatbot.controllers.resolvers.SkillPayloadResolver;
 import org.binchoo.paimonganyu.ikakao.SkillPayload;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
 
 /**
  * @author jbinchoo
  * @since 2022/06/11
  */
-@RequiredArgsConstructor
 @Component
-public class ActionParamResolver implements HandlerMethodArgumentResolver {
+public class ActionParamResolver extends RequestResponseBodyMethodProcessor {
 
     private final SkillPayloadResolver skillPayloadResolver;
+
+    public ActionParamResolver(List<HttpMessageConverter<?>> converters, SkillPayloadResolver skillPayloadResolver) {
+        super(converters);
+        this.skillPayloadResolver = skillPayloadResolver;
+    }
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -28,7 +38,9 @@ public class ActionParamResolver implements HandlerMethodArgumentResolver {
     }
 
     @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+    protected <T> Object readWithMessageConverters(NativeWebRequest webRequest, MethodParameter parameter, Type paramType)
+            throws IOException, HttpMediaTypeNotSupportedException, HttpMessageNotReadableException {
+
         ActionParam actionParam = parameter.getParameterAnnotation(ActionParam.class);
         SkillPayload skillPayload = skillPayloadResolver.resolve(webRequest.getNativeRequest(HttpServletRequest.class));
         if (skillPayload != null && actionParam != null) {
