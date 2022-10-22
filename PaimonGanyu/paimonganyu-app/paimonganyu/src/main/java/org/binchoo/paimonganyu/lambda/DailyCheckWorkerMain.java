7e42e6aedf98e4340f0fbf22ed9f82fad46673ed
binchoo <079111w@gmail.com>
2022-10-22 15:08:16 +0900

    Make DailyCheckWorkerLambda use RetryDailyCheckClientAdapter

--- PaimonGanyu/paimonganyu-app/paimonganyu/src/main/java/org/binchoo/paimonganyu/lambda/DailyCheckWorkerMain.java
+++ PaimonGanyu/paimonganyu-app/paimonganyu/src/main/java/org/binchoo/paimonganyu/lambda/DailyCheckWorkerMain.java
@@ -2,11 +2,11 @@ package org.binchoo.paimonganyu.lambda;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.binchoo.paimonganyu.dailycheck.driving.DailyCheckPort;
-import org.binchoo.paimonganyu.hoyoapi.DailyCheckSyncApi;
+import org.binchoo.paimonganyu.hoyoapi.DailyCheckAsyncApi;
 import org.binchoo.paimonganyu.hoyoapi.autoconfig.HoyoApiWebClientConfigurer;
 import org.binchoo.paimonganyu.infra.dailycheck.dynamo.repository.UserDailyCheckDynamoAdapter;
 import org.binchoo.paimonganyu.infra.dailycheck.dynamo.repository.UserDailyCheckDynamoRepository;
-import org.binchoo.paimonganyu.infra.dailycheck.web.DailyCheckClientAdapter;
+import org.binchoo.paimonganyu.infra.dailycheck.web.RetryDailyCheckClientAdapter;
 import org.binchoo.paimonganyu.lambda.config.DynamoDBClientConfig;
 import org.binchoo.paimonganyu.lambda.config.UserDailyCheckTableConfig;
 import org.binchoo.paimonganyu.service.dailycheck.DailyCheckService;
@@ -31,10 +31,10 @@ public class DailyCheckWorkerMain {
      * @param repository from {@link UserDailyCheckTableConfig}
      */
     @Bean
-    public DailyCheckPort dailyCheckService(DailyCheckSyncApi dailyCheckApi,
+    public DailyCheckPort dailyCheckService(DailyCheckAsyncApi dailyCheckApi,
                                             UserDailyCheckDynamoRepository repository) {
         return new DailyCheckService(
-                new DailyCheckClientAdapter(dailyCheckApi),
+                new RetryDailyCheckClientAdapter(dailyCheckApi),
                 new UserDailyCheckDynamoAdapter(repository));
     }
 }
