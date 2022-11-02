842d2a9c1c4d10cdf6dde421e10be800a579c91e
binchoo <079111w@gmail.com>
2022-11-02 10:29:01 +0900

    Add RedeemExhaustedException

---
+++ PaimonGanyu/paimonganyu-hoyoapi/src/main/java/org/binchoo/paimonganyu/hoyoapi/error/exceptions/RedeemExhaustedException.java
@@ -0,0 +1,12 @@
+package org.binchoo.paimonganyu.hoyoapi.error.exceptions;
+
+import org.binchoo.paimonganyu.hoyoapi.error.Retcode;
+import org.binchoo.paimonganyu.hoyoapi.error.RetcodeException;
+
+/**
+ * @author jbinchoo
+ * @since 2022/11/02
+ */
+@Retcode(codes = -2006)
+public class RedeemExhaustedException extends RetcodeException {
+}
